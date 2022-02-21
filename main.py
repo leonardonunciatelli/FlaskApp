99247a8addda3120048bc4606aab803956c8d899
leonardonunciatelli <leutn@hotmail.com>
2022-02-21 12:20:48 -0300

    Changed file path destination, Added CSV Reader and showing results as html table

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+
+import pandas as pd
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"] = os.path.join('repository')
 
 @app.route('/upload')
 def upload_file():
@@ -10,8 +17,16 @@ def upload_file():
 def uploader_file():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename))) 
+
+      return read_file()
+
+
+def read_file():
+    filename = 'repository/total_sales.csv'
+    data = pd.read_csv(filename, error_bad_lines=False, sep = ';', encoding="cp1252", header=0)
+    myData = data.values
+    return render_template('table.html', myData=myData)
 		
 if __name__ == '__main__':
    app.run(debug = True)
